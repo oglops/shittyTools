2a1ffef0f81572a752fd65aa15b29ddf187a1347
nobody <nobody@gmail.com>
2014-01-27 00:01:15 +0800

    add -ch fix

--- mayaFileConverter.py
+++ mayaFileConverter.py
@@ -29,6 +29,11 @@ def newName(f, args):
     return '{newName}{ext}'.format(**locals())
 
 
+def getVerBase(ver):
+    verBase = re.search('^\d+', ver).group()
+    return int(verBase)
+
+
 def main(args=None):
 
     for f in args.files:
@@ -44,36 +49,36 @@ def main(args=None):
 
                     verEndPos = m.find('UVER')
 
-                    currentVer = m[verEndPos-8:verEndPos]
+                    currentVer = m[verEndPos - 8:verEndPos]
                     print 'currentVer [%s]' % currentVer
                     if currentVer != args.v:
 
                         print f, '-->', newFile
-                        
-                        newMbFile.write(m[:verEndPos-8])
+
+                        newMbFile.write(m[:verEndPos - 8])
                         newMbFile.write(args.v)
 
-                        for i in range(8-len(args.v)):
+                        for i in range(8 - len(args.v)):
                             newMbFile.write(b'\x00')
 
                         newMbFile.write(m[verEndPos:])
 
-                        chunk = 1024*1024
+                        chunk = 1024 * 1024
                         while True:
                             piece = mbFile.read(chunk)
                             if not piece:
-                                print 'last run:' , piece 
+                                print 'last run:', piece
                                 break
                             newMbFile.write(piece)
 
                     else:
                         print f, '-->', 'skipped'
 
-
         elif f.endswith('.ma'):
 
             with open(f, "r") as maFile:
                 with open(newFile, "w") as newMaFile:
+                    verChange = False
                     for line in maFile:
 
                         if re.match('^requires maya ".*";\n', line):
@@ -82,16 +87,37 @@ def main(args=None):
                                 print f, '-->', newFile
                                 line = line.replace(currentVer, args.v, 1)
                                 newMaFile.write(line)
+                                verChange = True
                                 break
                             else:
                                 print f, '-->', 'skipped'
 
                         newMaFile.write(line)
 
-                    # we have replaced the line
-                    for line in maFile:
-                        newMaFile.write(line)
+                    # real scene content starts somewhere below this "requires
+                    # xxx" line of course
 
+                    hack = False
+                    if verChange:
+                        fileVer = getVerBase(currentVer)
+                        newVer = getVerBase(args.v)
+                        if fileVer > newVer and fileVer > 2012:
+                            hack = True
+
+                    # we have replaced the version line
+                    if verChange and hack:
+                        for line in maFile:
+
+                            # this is for the -ch fix
+                            # // Error: file: E:/shittyTools/2014_cube_2012.ma line 94: Invalid flag '-ch' //
+                            if re.match('\tsetAttr.*-ch \d+ .*', line):
+                                line = re.sub(' -ch \d+', '', line)
+
+                            newMaFile.write(line)
+                    # we have nothing to deal with, just write everything
+                    else:
+                        for line in maFile:
+                            newMaFile.write(line)
 
 
 def _parseArgs():
